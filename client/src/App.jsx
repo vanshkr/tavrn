08f473ad6d45e67a0fa286dda0c740d047053af6
vansh <krvansh30@gmail.com>
2025-04-05 01:26:34 +0530

    Project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+        Vnshop
+      </div>
+    </>
+  )
+}
+
+export default App
